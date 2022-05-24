c37ee33a37a0bf2addeec71dd7324d86fee22303
anthonytrillo <anthony.trillo.92@gmail.com>
2022-05-24 20:20:56 -0300

    Agrego ejercitaciones sobre funciones

---
+++ funciones.js
@@ -0,0 +1,16 @@
+// DESAFIOS USANDO FUNCIONES
+
+/**
+ * Crear una función que imprima por consola el resultado
+ * de la suma de dos números, numero1 y numero2.
+ */
+
+/**
+ * Pedir por medio de prompt dos números al usuario y guardar estos
+ * números en variables, por ejemplo numero1 y numero2,
+ * luego crear una función que reciba por parámetros esos dos números
+ * desde la invocación de la función, dentro de la función sumar esos
+ * números e imprimir la respuesta en la consola.
+ *
+ * Realizar este ejercicio con funciones regulares y con Arrow functions
+ */
