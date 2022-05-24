2de6dddcd474bf17c644d42add5976f7cf3cf924
anthonytrillo <anthony.trillo.92@gmail.com>
2022-05-24 19:30:02 -0300

    Ejercitación sobre condicionales

---
+++ condicionales.js
@@ -0,0 +1,14 @@
+// DESAFIO CON CONDICIONALES
+
+/**
+ * Crear un prompt que reciba un número y que tenga el texto:
+ * "Ingrese el stock del producto:", guardar ese número en una variable
+ * llamada stock.
+ */
+
+/**
+ * Crear un condicional que imprima por consola "El producto tiene stock" si
+ * el stock es mayor que 0 (cero) y "El producto no tiene stock" si es igual
+ * a 0 (cero).
+ */
+
